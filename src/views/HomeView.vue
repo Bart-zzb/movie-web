3783d21f5b350ea13d2ebd784934a318a45fb245
bart_zzb <3533272661@qq.com>
2024-02-19 22:37:47 +0800

    movie-web

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="common-layout">
     <el-container>
       <!-- 首页头部 -->
-      <el-header style="background-color: #fff;height: 80px; padding: 0;">
+      <el-header style="height: 80px; padding: 0;">
         <div class="header-div">
           <a href="/" class="header-div-a">豆豆</a>
           <a href="/" class="header-div-a">读书</a>
@@ -14,12 +14,25 @@
           <a href="/" class="header-div-a">FM</a>
           <a href="/" class="header-div-a">时间</a>
           <a href="/" class="header-div-a">豆品</a>
-          <span style="position: absolute;right:0;" >
+          <span style="position:absolute;right:0;">
             <a href="/" class="header-div-a">下载豆豆客户端</a>
             <a href="/" class="header-div-a">登录/注册</a>
           </span>
-
         </div>
+        <div style="background-color: #F0F3F5;">
+          <el-row :gutter="10">
+            <el-col :span="18">
+              <span style="color:#258DCD">豆豆电影</span>
+              <el-input placeholder="搜索电影、电视剧、综艺、影人" style="width: 350px;">
+                <template #append>
+                  <el-button :icon="Search"></el-button>
+                </template>
+              </el-input>
+            </el-col>
+            <el-col :span="6">10</el-col>
+          </el-row>
+        </div>
+
       </el-header>
       <el-main>Main</el-main>
       <el-footer>Footer</el-footer>
@@ -28,7 +41,7 @@
 </template>
 
 <script setup>
-
+import {Search} from '@element-plus/icons-vue'
 </script>
 
 <style>
