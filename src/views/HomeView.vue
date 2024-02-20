1677b774e3ba99479fabf8f6933dc61fbc44d2cc
BartZhao <3533272661@qq.com>
2024-02-20 11:06:44 +0800

    movie-web

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,26 +4,26 @@
       <!-- 首页头部 -->
       <el-header style="height: 80px; padding: 0;">
         <div class="header-div">
-          <a href="/" class="header-div-a">豆豆</a>
-          <a href="/" class="header-div-a">读书</a>
-          <a href="/" class="header-div-a">电影</a>
-          <a href="/" class="header-div-a">音乐</a>
-          <a href="/" class="header-div-a">同城</a>
-          <a href="/" class="header-div-a">小组</a>
-          <a href="/" class="header-div-a">阅读</a>
-          <a href="/" class="header-div-a">FM</a>
-          <a href="/" class="header-div-a">时间</a>
-          <a href="/" class="header-div-a">豆品</a>
+          <span><a href="/" class="header-div-a">豆豆</a></span>
+          <span><a href="/" class="header-div-a">读书</a></span>
+          <span><a href="/" class="header-div-a">电影</a></span>
+          <span><a href="/" class="header-div-a">音乐</a></span>
+          <span><a href="/" class="header-div-a">同城</a></span>
+          <span><a href="/" class="header-div-a">小组</a></span>
+          <span><a href="/" class="header-div-a">阅读</a></span>
+          <span><a href="/" class="header-div-a">FM</a></span>
+          <span><a href="/" class="header-div-a">时间</a></span>
+          <span><a href="/" class="header-div-a">豆品</a></span>
           <span style="position:absolute;right:0;">
             <a href="/" class="header-div-a">下载豆豆客户端</a>
             <a href="/" class="header-div-a">登录/注册</a>
           </span>
         </div>
         <div style="background-color: #F0F3F5;">
-          <el-row :gutter="10">
+          <el-row :gutter="10" style="width: 1200px;margin:0 auto;">
             <el-col :span="18">
-              <span style="color:#258DCD">豆豆电影</span>
-              <el-input placeholder="搜索电影、电视剧、综艺、影人" style="width: 350px;">
+              <span style="color:#258DCD;font-size: 30px;margin: 50px 100px 50px 50px;line-height:70px;" >豆豆电影</span>
+              <el-input placeholder="搜索电影、电视剧、综艺、影人" style="width: 500px;margin-bottom:10px;">
                 <template #append>
                   <el-button :icon="Search"></el-button>
                 </template>
