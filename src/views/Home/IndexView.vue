0533a35bc2f2ce8c037cf172069a16122507229a
BartZhao <3533272661@qq.com>
2024-02-21 11:05:25 +0800

    movie-web

--- src/views/Home/IndexView.vue
+++ src/views/Home/IndexView.vue
@@ -11,13 +11,13 @@
         </el-menu>
 
         <el-carousel indicator-position="none" interval="5000">
-          <el-carousel-item >
-            <el-row :gutter="10" >
-             <el-col :span="6" v-for="item in arr">
-               <el-card align="center">
-                 <img :src="arr.imgUrl" style="width: 130px;">
-                 <p style="font-size:15px;margin: 3px auto;text-align: center">{{arr.title}}</p>
-                 <p style="font-size:13px;margin: 3px auto;text-align: center">{{arr.score}}</p>
+          <el-carousel-item v-for="item in arr">
+            <el-row :gutter="10"  >
+             <el-col :span="6">
+               <el-card align="center" style="padding:0;">
+                 <img :src="item.imgUrl" style="width: 130px;height: 180px">
+                 <p style="font-size:15px;margin: 3px auto;text-align: center">{{item.title}}</p>
+                 <p style="font-size:13px;margin: 3px auto;text-align: center">{{item.score}}</p>
                  <el-button type="primary" size="small"> 选座购票</el-button>
                </el-card>
              </el-col>
@@ -36,7 +36,11 @@
 
   const arr = ref([
     {imgUrl:'/imgs/movie1.jpg',title:'开心超人之时空营救',score:'暂无评分'},
-    {imgUrl:'/imgs/movie2.jpg',title:'开心超人之时空营救',score:'暂无评分'},
+    {imgUrl:'/imgs/movie2.jpg',title:'热辣滚烫',score:'7.9'},
+    {imgUrl:'/imgs/movie3.jpg',title:'第二十条',score:'7.7'},
+    {imgUrl:'/imgs/movie4.jpg',title:'飞驰人生2',score:'7.7'},
+    {imgUrl:'/imgs/movie5.jpg',title:'破战',score:'3.2'},
+    {imgUrl:'/imgs/movie6.jpg',title:'年会不能停！',score:'8.1'},
   ]);
 </script>
 
